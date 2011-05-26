8e764cf75499eea147cad93c61640549638142ad
joostdevalk <joostdevalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-26 11:33:57 +0000

    Version 0.4.1, fixed some bugs, see changelog in readme.txt
    git-svn-id: http://svn.wp-plugins.org/wordpress-seo/tags/0.4.1@389330 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/wp-seo-admin.js
@@ -0,0 +1,74 @@
+jQuery(document).ready(function () {
+	jQuery("#disableexplanation").change(function() {
+		if (jQuery("#disableexplanation").is(':checked')) {
+			jQuery("p.desc").css("display","none");
+		} else {
+			jQuery("p.desc").css("display","block");
+		}
+	}).change();
+	jQuery("#enablexmlsitemap").change(function() {
+		if (jQuery("#enablexmlsitemap").is(':checked')) {
+			jQuery("#sitemapinfo").css("display","block");
+		} else {
+			jQuery("#sitemapinfo").css("display","none");
+		}
+	}).change();
+	jQuery("#enablexmlnewssitemap").change(function() {
+		if (jQuery("#enablexmlnewssitemap").is(':checked')) {
+			jQuery("#newssitemapinfo").css("display","block");
+		} else {
+			jQuery("#newssitemapinfo").css("display","none");
+		}
+	}).change();
+	jQuery("#enablexmlvideositemap").change(function() {
+		if (jQuery("#enablexmlvideositemap").is(':checked')) {
+			jQuery("#videositemapinfo").css("display","block");
+		} else {
+			jQuery("#videositemapinfo").css("display","none");
+		}
+	}).change();
+	jQuery("#cleanpermalinks").change(function() {
+		if (jQuery("#cleanpermalinks").is(':checked')) {
+			jQuery("#cleanpermalinksdiv").css("display","block");
+		} else {
+			jQuery("#cleanpermalinksdiv").css("display","none");
+		}
+	}).change();		
+});
+
+function wpseo_exportSettings() {
+	jQuery.post(ajaxurl, { 
+			action: 'wpseo_export_settings', 
+		}, function(data) { 
+			if (data) {
+				jQuery('#exportbutton').attr('href', data);
+				jQuery('#exportbutton').text('Download export file');
+			}
+		}
+	);
+}
+
+function setWPOption( option, newval, hide, nonce ) {
+	jQuery.post(ajaxurl, { 
+			action: 'wpseo_set_option', 
+			option: option,
+			newval: newval,
+			_wpnonce: nonce 
+		}, function(data) {
+			if (data)
+				jQuery('#'+hide).hide();
+		}
+	);
+}
+
+function wpseo_killBlockingFiles( nonce ) {
+	jQuery.post( ajaxurl, {
+		action: 'wpseo_kill_blocking_files',
+		_ajax_nonce: nonce
+	}, function(data) {
+		if (data == 'success')
+			jQuery('#blocking_files').hide();
+		else
+			jQuery('#block_files').html(data);
+	});
+}
---
+++ wp-seo.php
@@ -0,0 +1,84 @@
+<?php 
+/*
+Plugin Name: WordPress SEO
+Version: 0.4.1
+Plugin URI: http://yoast.com/wordpress/seo/
+Description: The first true all-in-one SEO solution for WordPress, including on-page content analysis, XML sitemaps and much more.
+Author: Joost de Valk
+Author URI: http://yoast.com/
+*/
+
+if ( version_compare(PHP_VERSION, '5.2', '<') ) {
+	if ( is_admin() && (!defined('DOING_AJAX') || !DOING_AJAX) ) {
+		require_once ABSPATH.'/wp-admin/includes/plugin.php';
+		deactivate_plugins( __FILE__ );
+	    wp_die( __('WordPress SEO requires PHP 5.2 or higher, as will WordPress 3.2 and higher. The plugin has now disabled itself. For more info, <a href="http://yoast.com/requires-php-52/">see this post</a>.') );
+	} else {
+		return;
+	}
+}
+
+define( 'WPSEO_VERSION', '0.4.1' );
+
+$pluginurl = plugin_dir_url(__FILE__);
+if ( preg_match( '/^https/', $pluginurl ) && !preg_match( '/^https/', get_bloginfo('url') ) )
+	$pluginurl = preg_replace( '/^https/', 'http', $pluginurl );
+define( 'WPSEO_FRONT_URL', $pluginurl );
+
+define( 'WPSEO_URL', plugin_dir_url(__FILE__) );
+define( 'WPSEO_PATH', plugin_dir_path(__FILE__) );
+define( 'WPSEO_BASENAME', plugin_basename( __FILE__ ) );
+
+require WPSEO_PATH.'inc/wpseo-functions.php';
+require WPSEO_PATH.'inc/class-rewrite.php';
+require WPSEO_PATH.'inc/class-widgets.php';
+require WPSEO_PATH.'inc/class-sitemaps.php';
+
+if ( !defined('DOING_AJAX') || !DOING_AJAX )
+	require WPSEO_PATH.'inc/wpseo-non-ajax-functions.php';
+	
+$options = get_wpseo_options();
+
+if ( is_admin() ) {
+	require WPSEO_PATH.'admin/ajax.php';
+	if ( !defined('DOING_AJAX') || !DOING_AJAX ) {
+		require WPSEO_PATH.'admin/yst_plugin_tools.php';
+		require WPSEO_PATH.'admin/class-config.php';
+		require WPSEO_PATH.'admin/class-metabox.php';		
+		require WPSEO_PATH.'admin/class-taxonomy.php';
+		if ( isset( $options['opengraph'] )  && $options['opengraph'] )
+			require WPSEO_PATH.'admin/class-opengraph-admin.php';
+	}
+} else {
+	require WPSEO_PATH.'frontend/class-frontend.php';
+	if ( isset($options['breadcrumbs-enable']) && $options['breadcrumbs-enable'] )
+		require WPSEO_PATH.'frontend/class-breadcrumbs.php';
+	if ( isset( $options['opengraph'] )  && $options['opengraph'] )
+		require WPSEO_PATH.'frontend/class-opengraph.php';
+}
+
+// Load all extra modules
+if ( !defined('DOING_AJAX') || !DOING_AJAX )
+	wpseo_load_plugins( WP_PLUGIN_DIR.'/wordpress-seo-modules/' );
+
+// Let's act as though this is AIOSEO so plugins and themes that act differently for that will fix do it for this plugin as well.
+if ( !class_exists('All_in_One_SEO_Pack') ) {
+	class All_in_One_SEO_Pack {
+		function All_in_One_SEO_Pack() {
+			return true;
+		}
+	}
+}
+
+function wpseo_maybe_upgrade() {
+	$options = get_option( 'wpseo' );
+
+	// version <= 0.3.5	
+	if ( ! isset( $options['version'] ) ) {
+		flush_rewrite_rules();
+		//$GLOBALS['wpseo_sitemaps']->delete_sitemaps();
+		$options['version'] = WPSEO_VERSION;
+		update_option( 'wpseo', $options );
+	}
+}
+add_action( 'admin_init', 'wpseo_maybe_upgrade' );
